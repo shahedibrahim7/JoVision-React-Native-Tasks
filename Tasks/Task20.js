691ccf052d6518c2136d895869a35a5929c80a78
shahed <shahedibrahim7@gmail.com>
2025-01-29 01:03:28 +0300

    Modified Task20

--- Tasks/Task20.js
+++ Tasks/Task20.js
@@ -5,16 +5,13 @@ import MyClassPage_Task20 from '../Components/MyClassPage_Task20';
 const Task20 = () => {
     const [showPage, setShowPage] = useState(false);
     function onButtonPress() {
-        setShowPage(true)
-    }
-
-    if (showPage) {
-        return <MyClassPage_Task20></MyClassPage_Task20>
+        setShowPage(!showPage)
     }
 
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Button title="Show" onPress={onButtonPress}> </Button>
+            <Button title={showPage ? "Hide" : "Show"} onPress={onButtonPress} > </Button>
+            {showPage ? <MyClassPage_Task20></MyClassPage_Task20> : null}
         </View>
     )
 }
