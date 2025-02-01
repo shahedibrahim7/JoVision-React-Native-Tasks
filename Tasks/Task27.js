58c8ed797b5c183f9c447f89667cf42aaa4c0853
shahed <shahedibrahim7@gmail.com>
2025-02-02 01:32:08 +0300

    Modified Task27

--- Tasks/Task27.js
+++ Tasks/Task27.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import {View, Text, Button, Image} from 'react-native'
+import {View, Text, Button, Image, Alert} from 'react-native'
 
 const cat = require('../Resources/cat.jpg')
 const dog = require('../Resources/dog.jpg')
@@ -8,13 +8,19 @@ const bird = require('../Resources/bird.jpg')
 const Task27 = () => {
     const [img, setImg] = useState(null)
 
+    const op = () => {
+        Alert.alert("Select an image to display: ", "Choose 1 for cat, 2 for dog, or 3 for bird", [
+            {text:"1", onPress: () => setImg(cat)},
+            {text:"2", onPress: () => setImg(dog)},
+            {text:"3", onPress: () => setImg(bird)},
+            {text:"Cancel", style: "cancel"}
+        ])
+    }
+
     return (
-        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', gap: 10}}>
-            <Text style={{fontSize: 20, marginBottom: 20}}>Select image to display: </Text>
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <Image source={img} style={{width: 200, height: 200, marginBottom: 20}} />
-            <Button title="1. Cat" onPress={() => setImg(cat)}></Button>
-            <Button title="2. Dog" onPress={() => setImg(dog)}></Button>
-            <Button title="3. Bird" onPress={() => setImg(bird)}></Button>
+            <Button title="Choose an image" onPress={op}></Button>
         </View>
     )
 }
