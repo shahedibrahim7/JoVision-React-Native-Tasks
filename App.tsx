27dadbe7316f56c424faffc5bdd9671cde1188e6
shahed <shahedibrahim7@gmail.com>
2025-02-03 02:12:27 +0300

    Render Task30

--- App.tsx
+++ App.tsx
@@ -15,6 +15,7 @@ import Task26 from './Tasks/Task26';
 import Task27 from './Tasks/Task27';
 import Task28 from './Tasks/Task28';
 import Task29 from './Tasks/Task29';
+import Task30 from './Tasks/Task30';
 
 
 export default function App() {
@@ -32,7 +33,8 @@ export default function App() {
         //<Task26></Task26>
         //<Task27></Task27>
         //<Task28></Task28>
-        <Task29></Task29>
+        //<Task29></Task29>
+        <Task30></Task30>
 
     )
 }
